f35d6efa6e0173a5d2d01308829f940ed8b12c75
Nawajish <itsnawajish020@gmail.com>
2026-04-10 12:29:55 +0530

    day-3

---
+++ BasicQues.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class BasicQues {
+    public void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        // Reverse a number
+        int num = 12345;
+        int num2 = 123445;
+
+        while(num>0){
+            int digit = num%10;
+            System.out.print(digit);
+            num/=10;
+        }
+
+        System.out.println();
+
+        //Method 2
+        int rev = 0;
+        while(num2>0){
+            int dig = num2%10;
+            rev = rev*10 + dig;
+            num2/=10;
+        }
+        System.out.print("reversed: "+rev);
+     
+        System.out.println();
+
+
+        
+        // break keyword
+        do{
+            System.out.print("enter a num: ");
+            int noi = sc.nextInt();
+            if(noi%10 == 00) break;
+            System.out.println(noi);
+        }while(true);
+
+
+
+        // continue
+        do{
+            System.out.print("pls enter a num: ");
+            int noii = sc.nextInt();
+
+            if(noii%10 == 0) continue;
+
+            System.out.println("entered num was: "+noii);
+        }while(true);
+    }
+}
--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,72 @@
 public class Pattern {
     public void main(String[] args){
         int n = 4;
+
+        // square
         for(int i = 1;i <=n; i++){
             for(int j = 1; j<=n;j++){
                 System.out.print("* ");
             }
             System.out.println();
+        }
+
+        // star pattern
+        System.out.println("Star Pattern");
+        int n2 = 4;
+        int i = 1;
+        while(i<=n2){
+            int j = 1;
+            while(j<=i) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+        //inverted star pattern
+        System.out.println("Inverted Star Pattern");
+        int k = 1, n3 = 6;
+        while(k<=n3){
+            int j = (n3-k)+1;
+            while(j>=1){
+                System.out.print("* ");
+                j--;
+            }
+            System.out.println();
+            k++;
+        }
+
+
+        // Half pyramid( 1, 12, 123...)
+        System.out.println("Half Pyramid");
+        int num4 = 5;
+        int i4 = 1;
+        while(i4<=num4){
+            int j = 1;
+            while(j<=i4){
+                System.out.print(" " +j);
+                j++;
+            }
+            System.out.println();
+            i4++;
+        }
+
+
+        // Character pattern A, BC, DEF...)
+        System.out.println("Half Pyramid");
+        int num5 = 5;
+        int i5 = 1;
+        char ch = 'A';
+        while(i5<=num5){
+            int j = 1;
+            while(j<=i5){
+                System.out.print(" "+ch);
+                ch++;
+                j++;
+            }
+            System.out.println();
+            i5++;
         }
     }
 }
